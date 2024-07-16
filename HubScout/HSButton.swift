288292b21c23ba17443beaa48bc218742b71d9a8
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2024-07-16 11:28:27 +0500

    feat: add reusable HSButton subclass of UIButton with customizations
    HS refers to the project name: HubScout

---
+++ HubScout/HSButton.swift
@@ -0,0 +1,37 @@
+//
+//  HSButton.swift
+//  HubScout
+//
+//  Created by Muhammad Asad Chattha on 16/07/2024.
+//
+
+import UIKit
+
+class HSButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+
+    init(backgrounColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.titleLabel?.backgroundColor = backgrounColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+
+
+    private func configure() {
+        layer.cornerRadius      = 10
+        titleLabel?.textColor   = .white
+        titleLabel?.font        = .preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
