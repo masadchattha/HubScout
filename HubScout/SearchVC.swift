e572037a23f7131ec927c047bd844c3dde141e8c
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2024-07-17 23:25:41 +0500

    feat(search): setup search screen

--- HubScout/SearchVC.swift
+++ HubScout/SearchVC.swift
@@ -9,7 +9,65 @@ import UIKit
 
 class SearchVC: UIViewController {
 
+    let logoImageView = UIImageView()
+    let usernameTextField = HSTextField()
+    let callToActionButton = HSButton(backgrounColor: .systemGreen, title: "Get Followers")
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+        configureLogoImageView()
+        configureTextField()
+        configureButton()
     }
 }
+
+
+// MARK: - Configuration Methods
+
+private extension SearchVC {
+
+    func configureLogoImageView() {
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "gh-logo")
+
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+
+
+    func configureTextField() {
+        view.addSubview(usernameTextField)
+
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            usernameTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+
+
+    func configureCallToActionButton() {
+        view.addSubview(callToActionButton)
+
+        NSLayoutConstraint.activate([
+            callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -80),
+            callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            callToActionButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            callToActionButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+}
+
