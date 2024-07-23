747396b9569c54c6dfc64a8aff40ace79279ecdb
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2024-07-23 15:07:06 +0500

    feat(search): add custom HSAvatarImageView and FollowerCell

---
+++ HubScout/Custom Views/Cells/FollowerCell.swift
@@ -0,0 +1,13 @@
+//
+//  FollowerCell.swift
+//  HubScout
+//
+//  Created by o9tech on 23/07/2024.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    static let reuseID = "FollowerCell"
+
+}
