64efff5907b1aa8a7d5ecc1d981840fd051a35ba
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2024-07-23 19:14:26 +0500

    feat(search): create FollowerCell

--- HubScout/Custom Views/Cells/FollowerCell.swift
+++ HubScout/Custom Views/Cells/FollowerCell.swift
@@ -9,5 +9,39 @@ import UIKit
 
 class FollowerCell: UICollectionViewCell {
     static let reuseID = "FollowerCell"
+    let avatarImageView = HSAvatarImageView(frame: .zero)
+    let usernamaLabel = HSTitleLabel(textAlignment: .center, fontSize: 16)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
 
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func set(follwer: Follower) {
+        usernamaLabel.text = follwer.login
+    }
+
+    func configure() {
+        let padding: CGFloat = 8
+        addSubview(avatarImageView)
+        addSubview(usernamaLabel)
+        translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            avatarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor),
+
+            usernamaLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+            usernamaLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            usernamaLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            usernamaLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
 }
