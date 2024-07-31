ee30ac2897522d018dd6de85e2a76beb25025ef4
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2024-07-31 23:29:38 +0500

    feat(profile): implement network manager method to fetch user info

--- HubScout/Screens/UserInfoVC.swift
+++ HubScout/Screens/UserInfoVC.swift
@@ -15,7 +15,7 @@ class UserInfoVC: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         configureNavBar()
-        print("Username: ", username)
+        getUserInfo(for: username)
     }
 
 
@@ -27,5 +27,20 @@ class UserInfoVC: UIViewController {
 
     @objc func dismissVC() {
         dismiss(animated: true)
+    }
+
+
+    func getUserInfo(for username: String) {
+        guard username.isNotBlank else { return }
+        NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
+            guard let self else { return }
+            
+            switch result {
+            case .success(let user):
+                dump(user)
+            case .failure(let error):
+                self.presentHSAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
     }
 }
