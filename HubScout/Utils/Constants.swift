50e46827431bb4ccd4c5112bf3966adf336945cd
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2024-08-02 06:10:39 +0500

    feat(profile): add ItemInfoView

--- HubScout/Utils/Constants.swift
+++ HubScout/Utils/Constants.swift
@@ -9,4 +9,8 @@ import Foundation
 
 enum SFSymbols {
     static let location = "mappin.and.ellipse"
+    static let repos = "folder"
+    static let gists = "text.alignleft"
+    static let following = "heart"
+    static let followers = "person.2"
 }
