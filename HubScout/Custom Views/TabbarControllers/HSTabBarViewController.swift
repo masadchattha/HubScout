bafae4b19157a32cac7572ef243f7d7e6955e2af
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2024-08-07 18:15:22 +0500

    refactor(tabbar): move TabBar related code from SceneDelegate to a separate HSTabBarViewController class

---
+++ HubScout/Custom Views/TabbarControllers/HSTabBarViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HSTabbarViewController.swift
+//  HubScout
+//
+//  Created by o9tech on 07/08/2024.
+//
+
+import UIKit
+
+class HSTabBarViewController: UITabBarController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().tintColor = .systemGreen
+        viewControllers                 = [createSearchNC(), createFavoritesNC()]
+    }
+
+
+    func createSearchNC() -> UINavigationController {
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        return UINavigationController(rootViewController: searchVC)
+    }
+
+
+    func createFavoritesNC() -> UINavigationController {
+        let favoritesListVC = FavoritesListVC()
+        favoritesListVC.title = "Favorites"
+        favoritesListVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        return UINavigationController(rootViewController: favoritesListVC)
+    }
+}
