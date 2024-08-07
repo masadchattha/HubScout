7bad3443c0c66c8dbf0a28226121ac88eb76f579
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2024-08-07 13:16:21 +0500

    feat(favorite): add FavoriteListCell and implement table view in Favorite screen

---
+++ HubScout/Custom Views/Cells/FavoriteCell.swift
@@ -0,0 +1,52 @@
+//
+//  FavoriteCell.swift
+//  HubScout
+//
+//  Created by o9tech on 07/08/2024.
+//
+
+import UIKit
+
+class FavoriteCell: UITableViewCell {
+    static let reuseID = "FavoriteCell"
+    let avatarImageView = HSAvatarImageView(frame: .zero)
+    let usernamaLabel = HSTitleLabel(textAlignment: .left, fontSize: 26)
+
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
+    }
+
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+
+    func set(favorite: Follower) {
+        usernamaLabel.text = favorite.login
+        avatarImageView.downloadImage(from: favorite.avatarUrl)
+    }
+
+
+    func configure() {
+        addSubview(avatarImageView)
+        addSubview(usernamaLabel)
+
+        accessoryType = .disclosureIndicator
+        let padding: CGFloat = 12
+
+        NSLayoutConstraint.activate([
+            avatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            avatarImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 60),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 60),
+
+            usernamaLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 24),
+            usernamaLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+            usernamaLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            usernamaLabel.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+}
--- HubScout/Screens/FavoritesListVC.swift
+++ HubScout/Screens/FavoritesListVC.swift
@@ -9,7 +9,82 @@ import UIKit
 
 class FavoritesListVC: UIViewController {
 
+    let tableView             = UITableView()
+    var favorites: [Follower] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureVC()
+        configureTableView()
+    }
+
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getFavorites()
+    }
+
+
+    func configureVC() {
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+
+    func configureTableView() {
+        view.addSubview(tableView)
+
+        tableView.frame      = view.bounds
+        tableView.rowHeight  = 80
+        tableView.delegate   = self
+        tableView.dataSource = self
+
+        tableView.register(FavoriteCell.self, forCellReuseIdentifier: FavoriteCell.reuseID)
+    }
+
+
+    func getFavorites() {
+        PersistenceManager.retrieveFavorites { [weak self] result in
+            guard let self else { return }
+
+            switch result {
+            case .success(let favorites):
+                self.updateUI(with: favorites)
+
+            case .failure(let error):
+                self.presentHSAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
+    }
+
+
+    func updateUI(with favorites: [Follower]) {
+        if favorites.isEmpty {
+            self.showEmptyStateView(with: "No Favorites?\n\nAdd one on the follower screen.", in: self.view)
+        } else {
+            self.favorites = favorites
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+                self.view.bringSubviewToFront(self.tableView)
+            }
+        }
+    }
+}
+
+
+// MARK: - UITableViewDelegate & UITableViewDataSource
+
+extension FavoritesListVC: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favorites.count
+    }
+
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteCell.reuseID, for: indexPath) as! FavoriteCell
+        let favorite = favorites[indexPath.row]
+        cell.set(favorite: favorite)
+        return cell
     }
 }
