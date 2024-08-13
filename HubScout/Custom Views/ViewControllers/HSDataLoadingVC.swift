40cc8aa29812a80ec9f2b226a42f0d5dc3ed6eba
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2024-08-13 18:14:25 +0500

    refactor: constrain `activityIndicatorView` to `containerView` instead of `view`

--- HubScout/Custom Views/ViewControllers/HSDataLoadingVC.swift
+++ HubScout/Custom Views/ViewControllers/HSDataLoadingVC.swift
@@ -24,8 +24,8 @@ class HSDataLoadingVC: UIViewController {
         activityIndicator.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
-            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            activityIndicator.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: containerView.centerYAnchor)
         ])
         activityIndicator.startAnimating()
     }
