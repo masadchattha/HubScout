086921880e7eb62a67335174387b6e08935515fb
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2024-08-18 02:37:03 +0500

    refactor(network): convert downloadImage method to async/await

--- HubScout/Managers/NetworkManager.swift
+++ HubScout/Managers/NetworkManager.swift
@@ -149,4 +149,20 @@ class NetworkManager {
 
         task.resume()
     }
+
+
+    func downloadImage(from urlString: String) async -> UIImage? {
+        let cacheKey = NSString(string: urlString)
+        if let image = cache.object(forKey: cacheKey) { return image }
+        guard let url = URL(string: urlString) else { return nil }
+
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            guard let image = UIImage(data: data) else { return nil }
+            cache.setObject(image, forKey: cacheKey)
+            return image
+        } catch {
+            return nil
+        }
+    }
 }
