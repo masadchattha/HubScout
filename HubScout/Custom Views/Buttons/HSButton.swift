4c2401c6f61f6da7af9c49a2b9b3d209ec3051d7
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2024-08-20 12:41:16 +0500

    fix(HSButton): correct systemImageName initialization in convenience initializer

--- HubScout/Custom Views/Buttons/HSButton.swift
+++ HubScout/Custom Views/Buttons/HSButton.swift
@@ -22,7 +22,7 @@ class HSButton: UIButton {
 
     convenience init(color: UIColor, title: String, systemImageName: String) {
         self.init(frame: .zero)
-        set(color: color, title: title, systemImageName: "")
+        set(color: color, title: title, systemImageName: systemImageName)
     }
 
 
